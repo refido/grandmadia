65d4077b49e7fe40972659c973b7897eadc1fa64
Jon Tor <zirehl12@gmail.com>
2022-11-16 23:23:49 +0700

    delete order from cart page

--- src/page/CartPage.jsx
+++ src/page/CartPage.jsx
@@ -12,6 +12,15 @@ const CartPage = () => {
     useEffect(() => {
         getOrders();
     }, []);
+    const deleteOrder = x => async () => {
+        try {
+            await axios.delete(`http://localhost:3001/orders/${x}`)
+        } catch (e) {
+            console.log(e)
+        } finally { //no matter success or fail
+            getOrders() // refresh
+        }
+    }
     return (
         <div>
             <h4>Tas Belanja</h4>
@@ -22,6 +31,11 @@ const CartPage = () => {
                         <p>{order.title}</p>
                         <p>{order.weight}</p>
                         <p>Rp {order.price}</p>
+                        <button>-</button>
+                        <span>count</span>
+                        <button>+</button>
+                        <button onClick={deleteOrder(order.id)}>Hapus</button>
+                        <p>Total Pesanan {order.index} : </p>
                     </div>
                 ))}
             </div>
